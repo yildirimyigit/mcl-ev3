2b77ee87d898b582faa09beca04facc7414083e3
yigit <yigyil@gmail.com>
2019-11-18 14:10:42 +0300

    bluetooth v1

--- robot/wai_robot.py
+++ robot/wai_robot.py
@@ -9,11 +9,23 @@ from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.motor import MoveDifferential, OUTPUT_B, OUTPUT_C, SpeedRPM
 from ev3dev2.wheel import EV3EducationSetTire
 from ev3dev2.sound import Sound
+from ev3dev.ev3 import *
 from time import sleep
+
+import socket
+import bluetooth
 
 
 class WhereAmI:
     def __init__(self):
+        self.host_bt_mac = "AC:FD:CE:82:1E:CC"
+        self.port = 1
+        self.backlog = 1
+        self.size = 1024
+        self.s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self.s.bind((self.host_bt_mac, self.port))
+        self.s.listen(self.backlog)
+
         self.us = UltrasonicSensor()
     def run(self):
         print('RUNNING...')
